ca61af59e5203a82ebc50bf72f611bc93e14edb4
Magda Hermann <m.d.hermannn@gmail.com>
2025-02-14 06:25:40 +0100

    delete meal from mealDetail

--- frontend/src/app/components/meal-details/meal-details.component.ts
+++ frontend/src/app/components/meal-details/meal-details.component.ts
@@ -5,6 +5,7 @@ import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MealDto } from '../../shared/models/meal.dto';
 import { environment } from '../../../enviroments/enviroments';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-meal-details',
@@ -16,7 +17,12 @@ import { environment } from '../../../enviroments/enviroments';
 export class MealDetailsComponent implements OnInit {
   public meal: MealDto | null = null;
 
-  public constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {}
+  public constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+    public authService: AuthService
+  ) {}
 
   public ngOnInit(): void {
     const mealId = this.route.snapshot.paramMap.get('id');
@@ -30,4 +36,43 @@ export class MealDetailsComponent implements OnInit {
   public goBack(): void {
     this.router.navigate(['/meal-list']);
   }
+
+  public async deleteMeal(): Promise<void> {
+    if (!this.meal) return;
+  
+    if (!this.authService.isAdmin()) {
+      alert("You don't have permission to delete meals.");
+      
+      return;
+    }
+  
+    const confirmDelete = window.confirm(`Are you sure you want to delete ${this.meal.name}?`);
+    if (!confirmDelete) return;
+  
+    const mealId = this.route.snapshot.paramMap.get('id');
+    if (!mealId) return;
+  
+    try {
+      await this.http
+        .delete(`${environment.apiUrl}/meals/${mealId}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        })
+        .toPromise();
+  
+      alert('Meal deleted successfully!');
+      this.router.navigate(['/meal-list']);
+    } catch (error) {
+      console.error('Error deleting meal:', error);
+      alert('Failed to delete meal. Please try again.');
+    }
+  }
+  
+
+  public getMealImage(mealId: number): string {
+    const imagePath = `assets/${mealId}.jpg`;
+    const backupImagePath = 'assets/backup.png';
+
+    return mealId <= 16 ? imagePath : backupImagePath;
+  }
+  
 }
